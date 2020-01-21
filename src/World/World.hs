f2ed99ce2f07e763ef6194636d4f8b793059a962
Addison Bean <addisonbean@gmail.com>
2020-01-21 17:18:53 -0600

    Remove Map.hs

--- src/World/World.hs
+++ src/World/World.hs
@@ -5,24 +5,29 @@ module World.World
   ) where
 
 import Control.Lens (makeLenses, view)
+import Data.Aeson (decodeFileStrict')
 import Data.Maybe (fromJust)
 
-import World.Scene.Scene (Scene, description)
-import World.Map (PokeMap(..), readScenesJson, mapFromScenes, scenes)
--- import GameState (GameState)
+import World.Scene.Scene (Scene, description, sceneId)
+import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 
+type Scenes = HashMap String Scene
+
 data World = World
-  { _map :: PokeMap
+  { _scenes :: Scenes
   , _currentSceneId :: String
   } deriving (Show)
 makeLenses ''World
 
+scenesFromList :: [Scene] -> Scenes
+scenesFromList = HM.fromList . map (\s -> (view sceneId s, s))
+
 loadWorld :: FilePath -> IO (Maybe World)
-loadWorld = fmap (fmap (flip World "start" . mapFromScenes)) . readScenesJson
+loadWorld = fmap (fmap (flip World "start" . scenesFromList)) . decodeFileStrict'
 
 currentScene :: World -> Scene
-currentScene w = fromJust $ HM.lookup (_currentSceneId w) (view scenes $ _map w)
+currentScene w = fromJust $ HM.lookup (_currentSceneId w) (_scenes w)
 
 look :: World -> String
 look = view description . currentScene
